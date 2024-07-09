8fd36d6d0e190256cf60bcbef3ac0fc0d453c1fb
lilianqiang <lilianqiang@bytedance.com>
2024-07-09 13:50:40 +0800

    [llm_perf] add meta-llama3-70b and related requirements

--- byte_infer_perf/llm_perf/backends/GPU/model_impl/__init__.py
+++ byte_infer_perf/llm_perf/backends/GPU/model_impl/__init__.py
@@ -12,9 +12,11 @@ import torch
 import torch.nn as nn
 
 from .gpu_chatglm2 import GPUChatGLM2
+from .gpu_meta_llama3 import GPUMetaLlama3
 
 from llm_perf.utils.logger import logger
 
 __all__ = {
-    "chatglm2": GPUChatGLM2
+    "chatglm2": GPUChatGLM2,
+    "meta_llama3": GPUMetaLlama3
 }
